313235e269f8a9f1739ee123ae7d25e979aba4f1
Nicolas FRADIN <fradinni@gmail.com>
2013-02-07 09:37:06 +0100

    update

--- mpm.bat
+++ mpm.bat
@@ -1,2 +1,18 @@
 @echo off
+::setlocal enableextensions,enabledelayedexpansion
+SET DRIVE=%~d0
+SET CDIR=%~p0
+SET INSTALL_PATH=%DRIVE%%CDIR%
+
+SET ORIGINAL_PATH = %cd%
+SET GROOVY_PATH=%INSTALL_PATH%groovy-2.1.00
+
+IF NOT EXIST "%GROOVY_PATH%" GOTO NO_GROOVY
+SET PATH=%PATH%;%GROOVY_PATH%\bin
+cd "%INSTALL_PATH%"
+
+:NO_GROOVY
+
 groovy -cp scripts\objects\ scripts\mpm.groovy %*
+
+cd %ORIGINAL_PATH%
